759126a34ce8c0118d93e80ac6e7da8f3be8553c
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2021-12-23 09:13:58 -0300

    export and import nodejs

---
+++ aula1/moduleClient.js
@@ -0,0 +1,11 @@
+const moduleA = require('./moduleA');
+const moduleB = require('./moduleB');
+
+
+ console.log(moduleA.bemVindo)
+ console.log(moduleB.boaNoite())
+ console.log(moduleA.ateLogo)
+ console.log(moduleB)
+ console.log(moduleA)
+
+ //Estudo sobre exportação e importação   no node
