5d05a01bbbaba691f528e2096a3900b6ceda7a4d
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2021-12-27 09:55:37 -0300

    desafio cod3r

--- funcionarios.js
+++ funcionarios.js
@@ -2,10 +2,29 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
 
+const chinesa = f => f.pais ==='China';// função que recebe funcionario como params e verifica se o pais é da china.
+
+const mulheres = f => f.genero === 'F';// função que recebe funcionario como params e verifica se o genero é feminino.
+
+const menoSalario = ( funcionario, funcionarioAtual ) => { 
+  return funcionario.salario > funcionarioAtual.salario ? funcionario : funcionarioAtual;
+}//metodo reduce para passar em cada funcionario e comparar se é true ou false e criar um novo array com essa informação.
+
 axios.get(url)
   .then(response => {
     const funcionarios = response.data
-    console.log(funcionarios);
-    console.log(funcionarios[0]);
-    console.log(funcionarios[0].nome);
-  })
+    //console.log(funcionarios)
+    
+    const func = funcionarios
+    .filter(chinesa)
+    .filter(mulheres)
+    .reduce(menoSalario)
+    console.log(func)
+
+})
+    
+       
+
+    
+    
+  
